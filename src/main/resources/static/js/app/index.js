152a2714cf6dab0802030b5832090e75fd7bb87b
setge <shyune@knou.ac.kr>
2021-01-11 00:06:05 +0900

    스프링 부트 게시판 CRUD 구현

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,6 +7,9 @@ let main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+        $('#btn-delete').on('click', function (){
+           _this.delete();
+        });
     },
     save: function () {
         let data = {
@@ -47,7 +50,23 @@ let main = {
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
+    },
+    delete: function (){
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert(id + '번 글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+
 };
 
 main.init();
